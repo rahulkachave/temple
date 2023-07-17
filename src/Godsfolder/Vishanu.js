d5417c628d9cafc934ddf621f651226c5f561b77
rahulkachave <rahulkachave4142@gmail.com>
2023-07-17 18:30:42 +0530

    dine

--- src/Godsfolder/Vishanu.js
+++ src/Godsfolder/Vishanu.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import ganesha5  from './/../staic/Green Watercolor Blank Paper A4 Document  (1).gif'
 const Vishanu = () => {
   return (
     <div>Vishanu
 
       <Link to={'/SignUp'}>SignUp</Link>
+      <div className="photo-item">   < video src={ganesha5} alt="Ganesha 5"/></div>
     </div>
     
   )
